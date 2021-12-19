4a353e9ed3cb6af97853f856d79e0e758caa65f4
Mike Pagé <mikepagenl@gmail.com>
2021-12-19 23:04:27 +0100

    feat: add datepicker popover panel

--- src/components/Datepicker.tsx
+++ src/components/Datepicker.tsx
@@ -1,11 +1,12 @@
 import { CalendarIcon } from '@heroicons/react/outline'
-import React from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
 
 interface Props {}
 
 const Datepicker = (props: Props) => {
   return (
-    <div className="relative">
+    <Popover className="relative">
       <input
         type="text"
         className="w-full py-3 pl-4 pr-10 font-medium leading-none text-gray-600 rounded-lg shadow-sm focus:outline-none focus:shadow-outline"
@@ -13,9 +14,24 @@ const Datepicker = (props: Props) => {
       />
 
       <div className="absolute top-0 right-0 px-3 py-2">
-        <CalendarIcon className="w-6 h-6 text-gray-400" />
+        <Popover.Button>
+          <CalendarIcon className="w-6 h-6 text-gray-400" />
+        </Popover.Button>
       </div>
-    </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-200"
+        enterFrom="opacity-0 translate-y-1"
+        enterTo="opacity-100 translate-y-0"
+        leave="transition ease-in duration-150"
+        leaveFrom="opacity-100 translate-y-0"
+        leaveTo="opacity-0 translate-y-1"
+      >
+        <Popover.Panel className="absolute z-10 mt-3 transform -translate-x-1/2 w-72 h-72 left-1/2">
+          <div className="w-full h-full overflow-hidden bg-white rounded-lg shadow-lg"></div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
   )
 }
 
